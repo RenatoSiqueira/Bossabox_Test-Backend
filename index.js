943d58a8b51d658860ae9a6a17b65daf565a7410
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-08 09:13:44 -0300

    Added Initial Database

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const initialDatabase = require('./initialDatabase')
 const toolsRouter = require('./routes/tools')
 
 const app = express()
@@ -12,12 +13,13 @@ const { PORT, MONGOSERVER } = process.env
 mongoose.Promise = global.Promise
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/tools', toolsRouter)
 
 mongoose
     .connect(MONGOSERVER, { useNewUrlParser: true })
-    .then(() => app.listen(PORT, () => console.log(`BossaBox Backend Server Running on port ${PORT}`)))
+    .then(() => {
+        initialDatabase()
+        app.listen(PORT, () => console.log(`BossaBox Backend Server Running on port ${PORT}`))
+    })
     .catch(err => console.log(err))
-
