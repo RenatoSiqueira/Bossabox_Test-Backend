2c8737578ec366650c2ca1beedcc7d7f30d6567c
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-08 09:14:24 -0300

    Added Patch Method

--- controllers/tools.js
+++ controllers/tools.js
@@ -2,24 +2,20 @@ const Tool = require('../models/tool')
 
 const find = async (req, res) => {
     const { tag } = req.query
-    const param = {}
+    let param = {}
 
     if (!!tag)
-        param = { tag }
+        param = { tags: tag }
 
     const tools = await Tool.find(param)
     res.json(tools)
 }
 
-const create = (req, res) => {
+const create = async (req, res) => {
     const tool = new Tool(req.body)
     try {
         await tool.save()
-
-        res
-            .status(201)
-            .send(tool)
-
+        res.status(201).json(tool)
     } catch (e) {
         res.send({
             success: false,
@@ -28,13 +24,21 @@ const create = (req, res) => {
     }
 }
 
+const patch = async (req, res) => {
+    const { id } = req.params
+    const tool = await Tool.updateOne({ _id: id }, req.body)
+    res.status(201).json(tool)
+}
+
 const remove = async (req, res) => {
-    await Tool.remove({ id })
-    res.status(204)
+    const { id } = req.params
+    await Tool.deleteOne({ _id: id })
+    res.status(204).json()
 }
 
 module.exports = {
     find,
     create,
+    patch,
     remove
 }
--- routes/tools.js
+++ routes/tools.js
@@ -5,7 +5,8 @@ const toolsController = require('../controllers/tools')
 
 router
     .get('/', toolsController.find)
-    .post('/', toolsController.find)
+    .post('/', toolsController.create)
+    .patch('/:id', toolsController.patch)
     .delete('/:id', toolsController.remove)
 
 module.exports = router
